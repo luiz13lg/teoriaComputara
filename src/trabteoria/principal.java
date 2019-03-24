f07a397c3d1f31ff1a2c13a9c5aeb6f45c5af185
luiz13lg <luiz13lg13@gmail.com>
2019-03-24 11:58:36 -0300

    Comparação entre números

--- src/trabteoria/principal.java
+++ src/trabteoria/principal.java
@@ -19,27 +19,25 @@ public class principal {
         int numB[];
         int soma[];
         
-        numA = iniciaReg(-6);
-        numB = iniciaReg(6);
-        
-        soma = somaReg(numA, numB);
-        
-        System.out.println(soma[0]);
-        System.out.println(soma[1]);
+        numA = iniciaReg(-5);
+        numB = iniciaReg(-4);
+
+        if(compReg(numA,numB) == 0) System.out.println("A é maior");
+            else System.out.println("B é maior");
     }
     
     private static int[] iniciaReg(int num){
         int auxNum[] = new int[2];
         
-        if(num >= 0){ 
+        if(num >= 0){
             auxNum[0] = 1;
             auxNum[1] = num;
         }
-        else{ 
+        else{
             auxNum[0] = 0;
             auxNum[1] = Math.abs(num);
         }
-        
+
         return auxNum;
     }
     
@@ -95,4 +93,26 @@ public class principal {
         return A;
     }
     
+    private static short compReg(int[] A, int [] B){    //se 0 A é maior, se 1 B é maior
+        if(A[0] == 0 && B[0] == 0){ //dois negativos
+            while(true){
+                A[1]--;
+                B[1]--;
+                if(A[1] == 0) return 0; //A é maior
+                if(B[1] == 0) return 1; //B é maior
+            }
+        }
+        
+        if(A[0] == 0 || B[0] == 0){  //um dos dois é negativo
+            if(A[0] == 0) return 1;
+            else return 0;
+        }
+        
+        while(true){
+            A[1]--;
+            B[1]--;
+            if(A[1] == 0) return 1; //B é maior
+            if(B[1] == 0) return 0; //A é maior
+        }
+    }
 }
